ef043630370f461b33ccd3b2929c3c7ff8c1241c
Andrey Gladyshev <Andrey_Gladyshev@epam.com>
2018-06-27 18:54:44 +0600

    Trying to load content from pod.

--- src/main/java/be/dela/logview/endpoints/ProcessEndpoint.java
+++ src/main/java/be/dela/logview/endpoints/ProcessEndpoint.java
@@ -2,19 +2,42 @@ package be.dela.logview.endpoints;
 
 import be.dela.logview.proc.WrappedProcess;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(path = "proc")
+@RequestMapping(path = "log")
 public class ProcessEndpoint {
 
+    private static Map<String, WrappedProcess> podLogs = new HashMap<>();
+
     @GetMapping
-    public Collection<String> readProcess() {
+    @RequestMapping(path = "pods")
+    public Collection<String> pods() {
         final WrappedProcess proc = new WrappedProcess("kubectl", "--kubeconfig=\"c:\\Work\\Dela\\kubectl\\kubeconfig-dev\"", "-n", "dev", "get", "pods");
-        return proc.flushToEnd();
+        final Collection<String> pods = proc.flushToEnd();
+        return pods.stream()
+                .skip(1)
+                .map(pod -> pod.split(" ")[0])
+                .collect(Collectors.toList());
     }
 
+    @GetMapping
+    @RequestMapping(path = "pods/{podName}")
+    public Collection<String> contentForPods(@PathVariable("podName") String podName) {
+        WrappedProcess pod = podLogs.get(podName);
+
+        if (pod == null) {
+            pod = new WrappedProcess("kubectl", "--kubeconfig=\"c:\\Work\\Dela\\kubectl\\kubeconfig-dev\"", "logs", "-f", "-v8", podName);
+            podLogs.put(podName, pod);
+        }
+
+        return pod.flush();
+    }
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,15 +1,42 @@
+var currentPod = null;
+
+function assignEventsToPodItems() {
+    $("#pod-item").click(
+        function (event) {
+            var podName = $(event.target).text();
+            $("#selected-pod").html(podName)
+            $("#output").html("Loading...");
+            $.ajax({
+                url: "/log/pods/" + podName,
+                success: function(result) {
+                    var resHtml = '<ul id="pod-item">' +
+                        result.map(function(pod) { return "<li>" + pod + "</li>" }).join("") +
+                        "</ul>";
+
+                    $("#pod-list").html(resHtml);
+
+                    assignEventsToPodItems();
+                }
+            });
+
+        }
+    );
+}
+
 $(function() {
 
-    $("#refresh-pods").click(function(event) {
+    $("#refresh-pods").click(function (event) {
         $("#pod-list").html("Loading...");
         $.ajax({
-            url: "/proc",
+            url: "/log/pods",
             success: function(result) {
-                var resHtml = "<ul>" +
+                var resHtml = '<ul id="pod-item">' +
                     result.map(function(pod) { return "<li>" + pod + "</li>" }).join("") +
                     "</ul>";
 
                 $("#pod-list").html(resHtml);
+
+                assignEventsToPodItems();
             }
         });
     });

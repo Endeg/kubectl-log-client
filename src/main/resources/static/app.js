275fbf367a37eba83b2f9bb38e7e8c9fdb7219f6
Andrey Gladyshev <Andrey_Gladyshev@epam.com>
2018-06-27 19:25:01 +0600

    Appending logs.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,23 +1,33 @@
 var currentPod = null;
 
+function pollForLogs() {
+    if (currentPod != null) {
+        setTimeout(function() {
+            $.ajax({
+                url: "/log/pods/" + currentPod,
+                success: function(result) {
+                    var currentText = $("#output").text() + "\n"
+                    $("#output").html(currentText + result.join("\n"));
+                    pollForLogs();
+                }
+            });
+        }, 1000);
+    }
+}
+
 function assignEventsToPodItems() {
     $("#pod-item").click(
         function (event) {
-            var podName = $(event.target).text();
-            $("#selected-pod").html(podName)
-            $("#output").html("Loading...");
-            $.ajax({
-                url: "/log/pods/" + podName,
-                success: function(result) {
-                    $("#output").html(result.join("\n"));
-                }
-            });
+            currentPod = $(event.target).text();
+            $("#selected-pod").html(currentPod)
+            $("#output-indicator").html("Loading...");
+            $("#output").html("");
+            pollForLogs();
         }
     );
 }
 
 $(function() {
-
     $("#refresh-pods").click(function (event) {
         $("#pod-list").html("Loading...");
         $.ajax({

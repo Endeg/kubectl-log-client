db2d3afda25667261307eaf0dcae89ea7db59700
Andrey Gladyshev <Andrey_Gladyshev@epam.com>
2018-06-27 14:38:49 +0600

    First commit.

---
+++ src/main/java/be/dela/logview/LogviewApplication.java
@@ -0,0 +1,12 @@
+package be.dela.logview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogviewApplication.class, args);
+	}
+}
